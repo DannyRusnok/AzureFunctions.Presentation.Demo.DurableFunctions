1e62b908275541a9ebcb9eb36b15f24eb17caba7
Daniel Rusnok <daniel.rusnok@itixo.com>
2021-06-20 05:57:20 +0200

    Hotfixes

---
+++ AzureFunctions.Presentation.Demo.DurableFunctions/MyNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureFunctions.Presentation.Demo.DurableFunctions
+{
+    public class MyNotification
+    {
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
